3e7386bc391cd2ec43f365209f77ed31bf44746a
mauricioppgo <47857286+mauricioppgo@users.noreply.github.com>
2019-06-14 19:16:15 -0500

    Add files via upload

---
+++ actividad.ts
@@ -0,0 +1,97 @@
+class Elevate {
+
+    private pisoNumero: Number; private elevacion: Number; private estado: boolean;
+
+
+    constructor()
+    {
+        this.pisoNumero=1; this.elevacion=5; this.estado=true;
+    }
+
+    getpisoNumero(pisoNumero)
+    {
+        return this.pisoNumero;
+    }
+
+    getelevacion(elevacion)
+    {
+        return this.elevacion;
+    }
+
+    getestado(estado)
+    {
+        return this.estado;
+    }
+
+    abrirPuerta(getestado)
+    {
+        if(getestado==false)
+        {
+            getestado==true;
+        }
+    }
+
+    cerrarPuerta(getestado)
+    {
+        if(getestado == true)
+        {
+            getestado == false;
+        } 
+    }
+    destino:number
+    subir(getpisoNumero, getelevacion, destino)
+    {
+        if(getpisoNumero = getelevacion)
+        {
+            console.log("Estas en el ultimo piso");
+        }
+        else if(destino > getpisoNumero)
+        {
+            do
+            {
+                getpisoNumero+=1
+                console.log(getpisoNumero);
+                
+            }while(destino > getpisoNumero);
+        }
+    }
+
+    bajar(getpisoNumero, getelevacion,destino)
+    {
+        if(getpisoNumero = getelevacion)
+        { 
+            console.log("Estas en el ultimo piso");
+        }
+        else if(destino < getpisoNumero)
+        {    
+        do
+        {
+            getpisoNumero-=1
+            console.log(getpisoNumero);
+            
+        }while(destino < getpisoNumero);
+        }
+    }
+
+    irA(destino,getpisoNumero)
+    {
+        if(destino < getpisoNumero)
+        {
+        do
+        {
+            getpisoNumero-=1
+            console.log(getpisoNumero);
+            
+        }while(destino < getpisoNumero);}
+
+        else if(destino>getpisoNumero)
+        {
+           do
+           {
+                getpisoNumero+=1
+                console.log(getpisoNumero);
+            
+            }while(destino > getpisoNumero);
+        }
+    }
+}
